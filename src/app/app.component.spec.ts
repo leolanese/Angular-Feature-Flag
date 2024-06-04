788c96aea0c4a4b68f78c0e11762d32419e629de
leolanese <laneseleo@gmail.com>
2024-06-04 07:11:41 +0100

    Updated demo img

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular_Feature-Flag');
+    expect(compiled.querySelector('h3')?.textContent).toContain('Hello, Angular_Feature-Flag');
   });
 });
